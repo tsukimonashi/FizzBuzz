b6dfa9155b182c791848fa4abaaf90fbe7af202d
tsukinashi <kyo.tsukimonashi@gmail.com>
2015-06-21 16:24:43 +0900

    ファイルの書き込み

--- request.py
+++ request.py
@@ -7,6 +7,8 @@ a = input()
 r = requests.get('http://connpass.com/api/v1/event/?keyword=%s'% a)
 
 print(r.status_code, r.headers['content-type'])
+f = open('save_events.txt' , 'w')
 
 for event in r.json()['events']:
     print(event['event_id'], event['title'])
+    f.write(str(event['event_id'])+'\n')
