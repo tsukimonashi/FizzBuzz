4b0d61e27bba374cc571731442970783ed08480b
tsukinashi <kyo.tsukimonashi@gmail.com>
2015-06-21 15:13:25 +0900

    FizzBuzz実装

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,3 +1,12 @@
 __author__ = 'tsukinashi'
 
-for i in range(30):
+a = int(input()) #目標値設定
+for i in range(a):  #iから目標値まで
+    if (i+1)%15 == 0:
+        print('FizzBuzz')
+    elif (i+1)%3 == 0:
+        print('Fizz')
+    elif (i+1)%5 == 0:
+        print('Buzz')
+    else:
+        print(i+1)
