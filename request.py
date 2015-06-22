51fb0c7371f85debf889585681e24ed77b14d740
KAZU <shaxon74@gmail.com>
2015-06-22 15:50:23 +0900

    request実装

--- request.py
+++ request.py
@@ -2,3 +2,11 @@ __author__ = 'tsukinashi'
 
 import requests
 
+a = input()
+
+r = requests.get('http://connpass.com/api/v1/event/?keyword=%s'% a)
+
+print(r.status_code, r.headers['content-type'])
+
+for event in r.json()['events']:
+    print(event['event_id'], event['title'])
